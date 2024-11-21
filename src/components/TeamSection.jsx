0e7ab797dde07d9d8a5f39d25d800957c1544af1
Rishu764 <rishabhbhatt218@gmail.com>
2024-11-21 20:37:21 +0530

    changes

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -15,11 +15,10 @@ const TeamSection = () => {
             alt="Dr. Kushal Pal Singh"
             className="w-24 h-24 rounded-full shadow-md mx-auto mb-4"
           />
-          <h3 className="text-lg font-semibold text-gray-800">Owner Name</h3>
-          <p className="title text-sm text-gray-600 my-2">Owner & CEO</p>
+          <h3 className="text-lg font-semibold text-gray-800">VIKASH PANCHOLI</h3>
+          <p className="title text-sm text-gray-600 my-2">DIRECTOR</p>
           <p className="description text-sm text-gray-700">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Dignissimos magnam nostrum asperiores laborum nulla vitae ipsam
+           
           </p>
         </div>
 
@@ -30,26 +29,10 @@ const TeamSection = () => {
             alt="Ashok Kumar Tyagi"
             className="w-24 h-24 rounded-full shadow-md mx-auto mb-4"
           />
-          <h3 className="text-lg font-semibold text-gray-800">CEO Name</h3>
-          <p className="title text-sm text-gray-600 my-2">CEO</p>
+          <h3 className="text-lg font-semibold text-gray-800">SIDDHI PANCHOLI</h3>
+          <p className="title text-sm text-gray-600 my-2">DIRECTOR</p>
           <p className="description text-sm text-gray-700">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Dignissimos magnam nostrum asperiores laborum nulla vitae ipsam
-          </p>
-        </div>
 
-        {/* Profile Card 3 */}
-        <div className="profile-card bg-white rounded shadow-md p-6 w-72 text-center hover:-translate-y-2 transition-transform duration-300 relative">
-          <img
-            src="https://www.pngkey.com/png/full/430-4304254_owner-icon-image-souleymane-wane.png"
-            alt="Devinder Singh"
-            className="w-24 h-24 rounded-full shadow-md mx-auto mb-4"
-          />
-          <h3 className="text-lg font-semibold text-gray-800">CEO Name</h3>
-          <p className="title text-sm text-gray-600 my-2">CEO</p>
-          <p className="description text-sm text-gray-700">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Dignissimos magnam nostrum asperiores laborum nulla vitae ipsam
           </p>
         </div>
       </div>
