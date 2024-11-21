1405694eaab3f2f467a0fad13173e7d334361706
ayush969696 <ayush972982@gmail.com>
2024-11-21 12:08:42 +0530

    adding logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa"; 
-import logo from "../assets/logo.png";
+import {logo} from "../asset/Logo.png";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
