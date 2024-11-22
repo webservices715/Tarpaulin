0bc76ba0f9950ccad62715eeaec458ba1012f419
Rishu764 <rishabhbhatt218@gmail.com>
2024-11-22 20:04:50 +0530

    changes

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-
+import director01 from "../assets/director01.jpeg";
+import director02 from "../assets/director02.jpeg";
 const TeamSection = () => {
   return (
     <section className="team-section bg-gray-50 py-24 text-center">
       <h2 className="text-2xl font-bold mb-10 text-gray-800">
-        Talented People Behind the Scenes of the Compay
+        DIRECTORS
       </h2>
       <div className="team-container flex flex-col gap-8 items-center justify-center md:flex-row">
         {/* Profile Card 1 */}
         <div className="profile-card bg-white rounded shadow-md p-6 w-72 text-center hover:-translate-y-2 transition-transform duration-300 relative">
           <div className="before:absolute before:top-0 before:left-0 before:h-full before:w-full before:rounded-lg before:border-4 before:border-transparent before:transition-all before:z-1 hover:before:border-image hover:before:border-image-source hover:before:border-image-slice[1]"></div>
           <img
-            src="https://www.pngkey.com/png/full/430-4304254_owner-icon-image-souleymane-wane.png"
-            alt="Dr. Kushal Pal Singh"
+            src={director01}
+            alt="Vikash Pancholi"
             className="w-24 h-24 rounded-full shadow-md mx-auto mb-4"
           />
           <h3 className="text-lg font-semibold text-gray-800">VIKASH PANCHOLI</h3>
@@ -25,8 +26,8 @@ const TeamSection = () => {
         {/* Profile Card 2 */}
         <div className="profile-card bg-white rounded shadow-md p-6 w-72 text-center hover:-translate-y-2 transition-transform duration-300 relative">
           <img
-            src="https://www.pngkey.com/png/full/430-4304254_owner-icon-image-souleymane-wane.png"
-            alt="Ashok Kumar Tyagi"
+            src={director02}
+            alt="Siddhi Pancholi"
             className="w-24 h-24 rounded-full shadow-md mx-auto mb-4"
           />
           <h3 className="text-lg font-semibold text-gray-800">SIDDHI PANCHOLI</h3>
