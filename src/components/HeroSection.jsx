6e3f7c5c6cdc79917ecd35d2b79c157ae670cde2
Rishu764 <rishabhbhatt218@gmail.com>
2024-11-23 10:07:34 +0530

    changes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -40,25 +40,13 @@ User's Phone Number: ${phoneNumber}`;
           <div className="max-w-lg mx-auto md:mx-0 text-center md:text-left">
             {/* Heading */}
             <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold md:mb-6 mb-4 md:leading-snug text-white">
-              Run on the
-              <span className="block md:py-2">rocky streets</span>
+              Satyakarta
             </h1>
-
-            {/* Subtitle */}
-            <p className="text-xl md:text-xl lg:text-2xl md:mb-8 text-white">
-              Fast Landing Delivery
-              <span className="block md:py-2 mb-10 md:mb-0">
-                For Your Goods Business
-              </span>
-            </p>
-
+           
             {/* Input Group */}
             <div className="flex items-center justify-center  md:justify-start w-full sm:w-11/12 md:w-3/4 lg:w-2/3 xl:w-1/2 mx-auto md:mx-0">
               <select className="h-16 lg:h-16 px-3 md:px-4 text-sm md:text-base lg:text-lg text-gray-600 border-none outline-none rounded-l-md">
                 {/* Country Codes */}
-                <option value="+1">+1 (US)</option>
-                <option value="+44">+44 (UK)</option>
-                <option value="+33">+33 (FR)</option>
                 <option value="+91">+91 (IN)</option>
               </select>
               <input
